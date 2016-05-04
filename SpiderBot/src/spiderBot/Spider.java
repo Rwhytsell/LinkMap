6b956b6f6187bd629c9ed269c8f163b29a54e215
Ryan Whytsell <Rwhytsell67@gmail.com>
2016-05-04 13:51:59 -0400

    Created GUI frame
    Created GUI frame as well as a placeholder label and button in seperate panels.

--- SpiderBot/src/spiderBot/Spider.java
+++ SpiderBot/src/spiderBot/Spider.java
@@ -1,10 +1,14 @@
 package spiderBot;
 
+/* Created by Ryan Whytsell, project started on 5/4/16 for educational purposes only*/
+
 public class Spider {
 
 	public static void main(String[] args) {
-		spiderBot.UI.start();
-
+		spiderBot.UI.start(); // Starts the UI
 	}
-
+	public void initSpider() //Will be called by the start button in the UI
+	{
+		//TODO Determine amount of threads, Link Depth, Origin URL from UI
+	}
 }
